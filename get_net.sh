34880d1aa797f525d59404da762c19e0a8f8d760
coolabhays <abhaysp9955@gmail.com>
2020-06-28 19:19:45 +0530

    fixed ssid

--- get_net.sh
+++ get_net.sh
@@ -20,34 +20,40 @@ fi
 logfile=${XDG_CACHE_HOME:-$HOME/.cache}/net/log
 prevdata="$(cat "${logfile}")"
 
-#sys_ether_name=$(nmcli device | awk '/ethernet/ && $3 == "connected" {print $1}')
-#sys_wifi_name=$(nmcli device | awk '/wifi/ && $3 == "connected" {print $1}')
-sys_ether_name=$(nmcli device | grep -E '\<ethernet\>.*\<connected\>' | cut -d ' ' -f1)
-sys_wifi_name=$(nmcli device | grep -E '\<wifi\>.*\<connected\>' | cut -d ' ' -f1)
+sys_ether_name=$(nmcli device | awk '/ethernet/ && $3 == "connected" {print $1}')
+sys_wifi_name=$(nmcli device | awk '/wifi/ && $3 == "connected" {print $1}')
+#sys_ether_name=$(nmcli device | grep -E '\<ethernet\>.*\<connected\>' | cut -d ' ' -f1)
+#sys_wifi_name=$(nmcli device | grep -E '\<wifi\>.*\<connected\>' | cut -d ' ' -f1)
 
 #echo ${sys_wifi_name}
-echo ${sys_ether_name}
+#echo ${sys_ether_name}
 
 
-wname=$(nmcli device show ${sys_wifi_name} | grep -i 'general.connection')
-ename=$(nmcli device show ${sys_wifi_name} | grep -i 'general.connection')
+#wname=$(nmcli device show ${sys_wifi_name} | grep -i 'general.connection')
+#ename=$(nmcli device show ${sys_wifi_name} | grep -i 'general.connection')
 
 #if [ "$PS1" != "" ]; then
 if [ -t 0 ] && [ -t 1 ]; then
 	if [ -n "${sys_wifi_name}" ] && [ -z "${sys_ether_name}" ]; then
-		#wifi_name=$(nmcli device show ${sys_wifi_name} \
-			#| awk 'BEGIN {IGNORECASE = 1} /general.connection/ {$1=""; print $0}')
+		wifi_name=$(nmcli device show ${sys_wifi_name} \
+			| awk 'BEGIN {IGNORECASE = 1} /general.connection/ {$1=""; print $0}')
 			#| awk 'BEGIN {IGNORECASE = 1} /general.connection/ { for (i=2; i<=NF; i++) printf "%s", $i }')
-		wifi_name=$(echo ${wname##* })
+		# wifi_name=$(echo ${wname##* })
 		printf "%s\n" "${wifi_name}"
 		printf "%s\n" "Ethernet Not Connected"
 	elif [ -n "${sys_ether_name}" ]  && [ -z "${sys_wifi_name}" ]; then
-		ether_name=$(echo ${ename##* })
+		# ether_name=$(echo ${ename##* })
+		ether_name=$(nmcli device show ${sys_ether_name} \
+			| awk 'BEGIN {IGNORECASE = 1} /general.connection/ {$1=""; print $0}')
 		printf "%s\n" "${ether_name}"
 		printf "%\ns" "Wifi Not Connected"
 	elif [ -n "${sys_wifi_name}" ] && [ -n "${sys_ether_name}" ]; then
-		wifi_name=$(echo ${wname##* })
-		ether_name=$(echo ${ename##* })
+		#wifi_name=$(echo ${wname##* })
+		#ether_name=$(echo ${ename##* })
+		wifi_name=$(nmcli device show ${sys_wifi_name} \
+			| awk 'BEGIN {IGNORECASE = 1} /general.connection/ {$1=""; print $0}')
+		ether_name=$(nmcli device show ${sys_ether_name} \
+			| awk 'BEGIN {IGNORECASE = 1} /general.connection/ {$1=""; print $0}')
 		printf "%s\n" "${wifi_name}"
 		printf "%s\n" "${ether_name}"
 	else
@@ -76,16 +82,24 @@ if [ -t 0 ] && [ -t 1 ]; then
 else
 	if [ -n "${sys_wifi_name}" ] && [ -z "${sys_ether_name}" ]; then
 			# print all columns accept 1
-		wifi_name=${wname##* }
+		#wifi_name=${wname##* }
+		wifi_name=$(nmcli device show ${sys_wifi_name} \
+			| awk 'BEGIN {IGNORECASE = 1} /general.connection/ {$1=""; print $0}')
 		notify-send --icon=~/.cache/notify-icons/radio-antenna.png -t 5000 "${wifi_name}
 Ethernet Not Connected"
 	elif [ -n "${sys_ether_name}" ]  && [ -z "${sys_wifi_name}" ]; then
-		ether_name=$(echo ${ename##* })
+		#ether_name=$(echo ${ename##* })
+		ether_name=$(nmcli device show ${sys_ether_name} \
+			| awk 'BEGIN {IGNORECASE = 1} /general.connection/ {$1=""; print $0}')
 		notify-send --icon=~/.cache/notify-icons/radio-antenna.png -t 5000 "${ether_name}
 Wifi Not Connected"
 	elif [ -n "${sys_wifi_name}" ] && [ -n "${sys_ether_name}" ]; then
-		wifi_name=$(echo ${wname##* })
-		ether_name=$(echo ${ename##* })
+		#wifi_name=$(echo ${wname##* })
+		#ether_name=$(echo ${ename##* })
+		wifi_name=$(nmcli device show ${sys_wifi_name} \
+			| awk 'BEGIN {IGNORECASE = 1} /general.connection/ {$1=""; print $0}')
+		ether_name=$(nmcli device show ${sys_ether_name} \
+			| awk 'BEGIN {IGNORECASE = 1} /general.connection/ {$1=""; print $0}')
 		notify-send --icon=~/.cache/notify-icons/radio-antenna.png -t 5000 "${wifi_name}
 ${ether_name}"
 	else
