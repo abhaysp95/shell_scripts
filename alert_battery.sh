abb9f2796c8c40737561e96672f1f56fe201c50d
coolabhays <abhaysp9955@gmail.com>
2020-08-09 11:06:04 +0530

    battery alert working

--- alert_battery.sh
+++ alert_battery.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
+# make the user enter the critical level
+# and check whether the critical_status function is working or not
+
+# check for the notification id, so new notification should replace the old one
+# instead of opening a new one
+
 bsfile="$HOME/.cache/temp/bsfile"
 if [ ! -f "${bsfile}" ]; then
 	touch "$bsfile"
-	echo "Charged" > "${bsfile}"
+	echo "0 Charged" > "${bsfile}"
 fi
 
 function get_help() {
@@ -18,25 +24,47 @@ function get_help() {
 }
 
 function general_status() {
-	local flag=$(cat "${bsfile}")
+	local flag=$(cut -d' ' -f 2 "${bsfile}")
+	local crit_stat=$(cut -d' ' -f 3 "${bsfile}")
 	if [ "${status_bat}" != "${flag}" ]; then
-		echo "${status_bat}" > "${bsfile}"
+		if [ -z "${crit_stat}" ]; then
+			echo "0 ${status_bat}" > "${bsfile}"
+		else
+			echo "0 ${status_bat} ${crit_stat}" > "${bsfile}"
+		fi
 		if [ ${status_bat} = "Discharging" ]; then
 			echo "Battery is now Discharging"
 			notify-send -t 7500 -i "$HOME/.cache/notify-icons/battery_disconnected.png" "Battery is now ${status_bat}" "Current level: ${capacity}"
 		elif [ "${status_bat}" = "Charging" ]; then
 			echo "Battery is now Charging"
 			notify-send -t 7500 -i "$HOME/.cache/notify-icons/battery_charging.png" "Battery is now ${status_bat}" "Current level: ${capacity}"
-		elif [ "${status_bat}" = "Charged" ]; then
+		elif [ "${status_bat}" = "Full" ]; then
 			echo "Battery is ${status_bat}"
-			notify-send -t 7500 -i "$HOME/.cache/notify-icons/battery_charging.png" "Battery is fully ${status_bat}" "Current level: ${capacity}"
+			notify-send -t 7500 -i "$HOME/.cache/notify-icons/battery_charging.png" "Battery is now ${status_bat}" "Current level: ${capacity}"
 		fi
 	fi
 }
 
 function critical_status() {
-	if [ "${capacity}" -le 30 ] && [ "${status_bat}" = "Discharging" ]; then
-		notify-send --urgency=critical -t 10000 -i "$HOME/.cache/notify-icons/battery.png" "Battery level, Please plug the charger" "Current Battery-level: ${capacity}%"
+	local crit_stat=$(cut -d' ' -f 3 "${bsfile}")
+	if [ -z "${crit_stat}" ]; then
+		crit_stat=30
+	fi
+	if [ "${capacity}" -le "${crit_stat}" ] && [ "${status_bat}" = "Discharging" ]; then
+		# replace notification with charging one, so that sleep doesn't effect it's showing
+		notify-send --urgency=critical -t 10000 -i "$HOME/.cache/notify-icons/battery_low.png" "Battery level, Please plug the charger" "Current Battery-level: ${capacity}%"
+		count=$(cut -d' ' -f 1 ${bsfile})
+		while true; do
+			if [ "${status_bat}" != "Charging" ]; then
+				count=$(( count + 1 ))
+				echo "${count} ${status_bat} ${crit_stat}" > "${bsfile}"
+				if [ "${count}" -eq 59 ]; then
+					echo "0 ${status_bat}" "${crit_stat}"> "${bsfile}"
+					break
+				fi
+				sleep 1
+			fi
+		done
 	fi
 }
 
@@ -47,15 +75,19 @@ for battery in /sys/class/power_supply/BAT?; do
 	case "$1" in
 		--help)
 			get_help
+			# break ;;
 			exit 0 ;;
 		--stat)
 			general_status
 			critical_status
+			# break ;;
+			exit 0 ;;
+		--critical)
+			echo "0 some_status $2" > "${bsfile}"
 			exit 0 ;;
 		*)
 			critical_status
+			# break ;;
 			exit 0 ;;
 	esac
-
-
 done
---
+++ alert_battery_wrapper.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+while true; do
+	alert_battery.sh --stat
+	# echo "done"
+	sleep 1
+done
